77be6d836cbdf687fa291149fdda1f8db007ecd4
allenjj98 <143020153+allenjj98@users.noreply.github.com>
2024-05-16 13:27:43 +0100

    Add method that selects a random word
    The list of words that are initially loaded are
    stored in a txt file, in which, each word is placed
    on a new line. For this reason, /n had to be removed
    before returning the secret word.

--- hangman.rb
+++ hangman.rb
@@ -1,9 +1,23 @@
 
-
 class Game
+    attr_reader :dict_words, :secret_word
+
     def initialize
         @dict_words = File.readlines("google-10000-english-no-swears.txt")
+        @secret_word = random_select_word
     end
+
+    # need to randomly select a word from the dictionary. This word has to be between 5 and 12 characters.
+    def random_select_word
+        filtered_dict = dict_words.select do |word|
+            word.length >= 5 and word.length <= 12
+        end
+        filtered_dict.sample.chomp
+    end
+
+
 end
 
 
+play = Game.new()
+p play.secret_word
