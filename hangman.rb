dcfb2595fa126deb565b52418a4c6106c83e43df
allenjj98 <143020153+allenjj98@users.noreply.github.com>
2024-05-16 13:43:04 +0100

    Fix randon word bug
    As the words contained a \n, the method was
    incorrectly selecting four letter words.

--- hangman.rb
+++ hangman.rb
@@ -5,14 +5,23 @@ class Game
     def initialize
         @dict_words = File.readlines("google-10000-english-no-swears.txt")
         @secret_word = random_select_word
+
+
+        @guesses = 0
+        @incorrect_guesses = []
     end
 
     # need to randomly select a word from the dictionary. This word has to be between 5 and 12 characters.
     def random_select_word
         filtered_dict = dict_words.select do |word|
-            word.length >= 5 and word.length <= 12
+            word.chomp.length >= 5 and word.chomp.length <= 12
         end
         filtered_dict.sample.chomp
+    end
+
+    # display the letters correctly guessed by the user. When initialised, this needs to represent the length of the secret words with underscores.
+    def display_correct_guesses
+        p secret_word.length
     end
 
 
@@ -21,3 +30,5 @@ end
 
 play = Game.new()
 p play.secret_word
+play.display_correct_guesses
+
